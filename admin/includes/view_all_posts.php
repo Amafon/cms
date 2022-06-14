768ac562ba85264f578de1ad151a955f631d6485
Amafon <afy.amafon@gmail.com>
2022-06-14 06:45:15 +0000

    Console

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -149,6 +149,7 @@ if (isset($_POST['checkBoxArray'])) {
 </form>
 
 <script>
+  console.log('Test');
   document.querySelector('.delete_link').addEventListener('click', function() {
     alert("It works!");
   })
