160b74546a7a457e4f6ec2f0e47a1f02d84f4d23
Amafon <afy.amafon@gmail.com>
2022-06-17 19:57:56 +0000

    Modal

--- admin/includes/delete_modal.php
+++ admin/includes/delete_modal.php
@@ -1,7 +1,7 @@
-<!-- Button to Open the Modal -->
+<!-- Button to Open the Modal
 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
   Open modal
-</button>
+</button> -->
 
 <!-- The Modal -->
 <div class="modal" id="myModal">
